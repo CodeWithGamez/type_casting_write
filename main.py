82403ba6f47c72bcb642ecbf1ae69cf793fe9946
CodeWithGamez <codewithgamez@gmail.com>
2020-08-23 05:17:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Write code for using TYPE CASTING
+
+
+
+#1 Make a variable of an interger and type cast it to a string and print it
+
+
+
+
+
+
+
+#2 Make a variable of a float and type cast it to an interger and a string and print them
+
+
+
+
+
+
+
+#3 Make a variable of a string and type cast it to an interger and print it
+
+
+
